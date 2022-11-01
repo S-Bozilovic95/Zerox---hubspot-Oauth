ffc368fc91ceb672198b1c1efa4d9482fe45f7f4
Lacedaimon95 <svetozar95@gmail.com>
2022-11-01 14:42:58 +0100

    master/ corrections

--- src/containers/LoggingScreen.tsx
+++ src/containers/LoggingScreen.tsx
@@ -1,21 +1,25 @@
-import { Button, Typography } from "@mui/material";
+import { FC } from "react";
+import { PlaylistAddCheckRounded } from "@mui/icons-material";
+import { Button, Stack, Typography } from "@mui/material";
 import { Box, Container } from "@mui/system";
 import logo from "assets/img/hubLogo.svg";
-import icon from "assets/img/Rectangle.svg";
 
-export const LoggingScreen: React.FC = () => {
+export const LoggingScreen: FC = () => {
   return (
     <Box>
       <Container
         sx={{
-          padding: "77px 0px 110px",
+          padding: "77px 10px 110px 10px",
           height: "100vh",
           display: "flex",
           flexDirection: "column",
           justifyContent: "flex-start",
           alignItems: "center",
           color: "#414141",
-          fontFamily: "Inter",
+
+          "@media (max-width: 1024px)": {
+            padding: "77px 10px 50px 10px ",
+          },
         }}
       >
         <img src={logo} />
@@ -23,10 +27,15 @@ export const LoggingScreen: React.FC = () => {
           <Typography
             mt="95px"
             variant="h1"
+            fontWeight="bold"
             sx={{
               fontSize: "43px",
-              lineHeight: "52.04px",
-              fontWeight: "700",
+
+              "@media (max-width: 1024px)": {
+                fontSize: "25px",
+                textAlign: "center",
+                marginTop: "65px",
+              },
             }}
           >
             Request for OAuth Integration:
@@ -34,54 +43,94 @@ export const LoggingScreen: React.FC = () => {
           <Typography
             variant="h3"
             mt="35px"
+            ml="27px"
             sx={{
               fontSize: "24px",
-              lineHeight: "29px",
-              fontWeight: "400",
               color: "#A8A8A8",
+
+              "@media (max-width: 1024px)": {
+                fontSize: "20px",
+                marginTop: "25px",
+                marginLeft: "0",
+                textAlign: "center",
+              },
             }}
           >
             <Typography
               component={"span"}
               variant="inherit"
-              ml="27px"
-              sx={{ fontWeight: "700", color: "#414141" }}
+              fontWeight="bold"
+              sx={{ color: "#414141" }}
             >
               Application{" "}
             </Typography>
             will send access request for:
           </Typography>
-          <Box
+          <Stack
+            direction="row"
+            alignItems="center"
             ml="27px"
             mt="35px"
             mb="54px"
-            sx={{ display: "flex", alignItems: "center" }}
+            sx={{
+              "@media (max-width: 1024px)": {
+                marginBottom: "45px",
+                marginLeft: "0",
+              },
+            }}
           >
-            <img src={icon} />
-            <Box ml="12px" sx={{ fontSize: "18px", lineHeight: "22px" }}>
-              <Typography sx={{ fontWeight: "700" }}>
+            <PlaylistAddCheckRounded
+              sx={{
+                color: "#959595",
+                padding: "7px",
+                fontSize: "55px",
+                backgroundColor: "#D9D9D9",
+                borderRadius: "5px",
+              }}
+            />
+            <Box
+              ml="12px"
+              sx={{
+                fontSize: "18px",
+
+                "@media (max-width: 768px)": {
+                  fontSize: "15px",
+                  textAlign: "center",
+                },
+              }}
+            >
+              <Typography fontWeight="bold">
                 User and Account Information
               </Typography>
-              <Typography sx={{ fontWeight: "400", color: "#A8A8A8" }}>
+              <Typography sx={{ color: "#A8A8A8" }}>
                 This includes access to read information about the user and
                 account
               </Typography>
             </Box>
-          </Box>
+          </Stack>
           <Button
             sx={{
               backgroundColor: "#EF8162",
               padding: "15px 28px",
+              display: "inline-block",
+              marginLeft: "21px",
+              marginTop: "10px",
               color: "#FFF",
               textTransform: "none",
               textDecoration: "none",
               fontSize: "16px",
-              lineHeight: "19px",
               fontWeight: "700",
 
               "&:hover": {
                 backgroundColor: "#ee8d73",
                 cursor: "pointer",
+              },
+
+              "@media (max-width: 768px)": {
+                fontSize: "15px",
+                marginTop: "10px",
+                marginLeft: "0px",
+                marginBottom: "10px",
               },
             }}
           >
